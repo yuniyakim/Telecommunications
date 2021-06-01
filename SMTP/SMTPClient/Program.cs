2e679c375829fe0c0f0bc2981424de5527fae583
Yuniya Kim <kim.yuniya@mail.ru>
2021-06-01 13:11:10 +0300

    Added basic SMTP client

---
+++ SMTP/SMTPClient/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SMTPClient
+{
+    public class Program
+    {
+        private const int port = 25;
+        private const string server = "localhost";
+
+        public static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Establishing connection...");
+                var client = new TcpClient();
+                client.Connect(server, port);
+
+                var data = new byte[128];
+                var response = new StringBuilder();
+                var stream = client.GetStream();
+                Console.WriteLine("Connection established.");
+
+                var message = "";
+                var numberOfBytes = 0;
+                do
+                {
+                    do
+                    {
+                        numberOfBytes = stream.Read(data, 0, data.Length);
+                    }
+                    while (numberOfBytes == 0);
+                    Console.WriteLine(new UTF8Encoding().GetString(data, 0, numberOfBytes));
+                    message = Console.ReadLine();
+                    var messageBytes = new UTF8Encoding().GetBytes(message);
+                    stream.Write(messageBytes, 0, messageBytes.Length);
+                    stream.Flush();
+                    numberOfBytes = 0;
+                } while (message != "QUIT" && message != "quit");
+
+                numberOfBytes = stream.Read(data, 0, data.Length);
+                Console.WriteLine(new UTF8Encoding().GetString(data, 0, numberOfBytes));
+
+                stream.Close();
+                client.Close();
+                Console.WriteLine("Connection closed.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
